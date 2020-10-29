56b3508834123724ba5bbc4ec23b0cc425356928
Thomas <thomaslee9474@gmail.com>
2020-10-29 16:16:27 -0700

    [update] stack

--- python/stack/stack.py
+++ python/stack/stack.py
@@ -1,7 +1,8 @@
 class Stack(object):
     """
         Stack implementation with list. The top of
-        the stack is the end of the list, the bottom of the stack is the beginning.
+        the stack is the end of the list, the bottom 
+        of the stack is the beginning.
 
         push - prepend, O(1)
         pop - change pointer of head, O(1)
@@ -10,29 +11,35 @@ class Stack(object):
 
     def __init__(self, iterable=None):
         self.stack = list()
+        self.size = 0
 
         if iterable:
             for data in iterable:
-                self.stack.append(data)
+                self.push(data)
 
     def is_empty(self):
-        if len(self.stack) < 1:
-            return True
-        return False
-
-    def length(self):
-        return len(self.stack)
+        return self.size == 0
 
     def push(self, item):
         self.stack.append(item)
+        self.size += 1
 
     def pop(self):
-        if len(self.stack) == 0:
+        if self.size == 0:
             raise ValueError('Linked list length is 0')
 
+        self.size -= 1
         return self.stack.pop()
 
     def peek(self):
         if self.is_empty():
             return None
         return self.stack[-1]
+
+
+obj = Stack([1, 5, 8])
+obj.push(11)
+res = obj.pop()
+print(obj.peek())
+
+
